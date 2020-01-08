2040262c92a4b5e7a798eab9775774678173e63a
Nicolas Plaisant <33159025+nicolasbcp@users.noreply.github.com>
2020-01-08 15:14:02 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,11 @@
+public class Teste {
+	public static void main (String[] args) {
+		ControleRemoto c = new ControleRemoto();
+		c.ligar();
+		c.maisVolume();
+		c.ligarMudo();
+		c.desligarMudo();
+		c.abrirMenu();
+		c.fecharMenu();
+	}
+}
